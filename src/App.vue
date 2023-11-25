6ec6909dcb9adbc6e7953383d61c91f6a3fe200d
Cristian Ayala <cristian.ayala@ulinksolution.com>
2023-11-24 19:35:48 -0600

    boilerplate

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <RouterView></RouterView>
+  </div>
+</template>
+
+<script>
+import { RouterView } from 'vue-router'
+
+export default {
+  components: { RouterView },
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  mounted() {},
+  methods: {}
+}
+</script>
+
+<style scoped></style>
